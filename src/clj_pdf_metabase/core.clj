f23d8f71d5c24b4a6fb056043fcf3610aa401426
Gaurav Panwar <g-p@live.in>
2019-12-06 14:29:01 +0530

    Basic Excel creation.

--- src/clj_pdf_metabase/core.clj
+++ src/clj_pdf_metabase/core.clj
@@ -1,10 +1,20 @@
 (ns clj-pdf-metabase.core
   (:use clj-pdf.core)
+  (:require [dk.ative.docjure.spreadsheet :as spreadsheet])
   (:gen-class))
+
+  
 
 ; (def msg1
 ;     (println "Hello GP!")
 ;     )
+
+(defn export-to-xlsx-file
+  "Write an XLS file to `file` with the header a and rows found in `results`"
+[]
+          (println "Starting export-to-xlsx-file...")
+          (spreadsheet/create-workbook "Query result")
+          (spreadsheet/save-workbook! "foo.xlsx"))
 
 (defn pdf-table [column-widths & rows]
   (into
@@ -15,6 +25,20 @@
   "I don't do a whole lot ... yet."
   [& args]
   ; (msg1)
+  (let [
+    column-names ["Name" "Price"]
+    rows ["Foo Widget" 100]
+    wb (spreadsheet/create-workbook "Price List"
+    [["Name" "Price"]
+     ["Foo Widget" 100]
+     ["Bar Widget" 200]])
+    ; sheet (spreadsheet/select-sheet "Price List" wb)
+    ; header-row (first (spreadsheet/row-seq sheet))
+    ]
+    ; (spreadsheet/set-row-style! header-row (spreadsheet/create-cell-style! wb {:background :yellow,
+    ;                               :font {:bold true}}))
+    (spreadsheet/save-workbook! "foo.xlsx" wb))
+
   (println "Starting...")
   ; (pdf
   ;   [{}
@@ -31,6 +55,7 @@
     ; [:table {:header ["Name" "ID" "Make" "Model" "Year"]}]
     ; ]
     [:table      
+    {:header ["c1" "c2" "c3"]}
       ["v11" "v12" "v13"]
       ["v21" "v22" "v23"]]
   ]
@@ -41,6 +66,10 @@
   ;   ["foo" [:chunk {:style :bold} "bar"] [:phrase "baz"]]
   ;   ["foo" "foo" "foo"]
   ;   ["foo" "foo" "foo"])
+
+
+  ; (spreadsheet/create-workbook "Query result")
+  ; (spreadsheet/save-workbook! "foo.xlsx")
 
   (println "Stopping...")
   )
